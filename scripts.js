2123511a173d8b92e89c4554021600cb2c6d407b
Jorge Muñoz Aguado <jorge.munnoza@estudiante.uam.es>
2018-10-15 19:37:05 +0200

    Now you can eat the blue pixels. When you eat that pixels your snake will grow in one point.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+
 var CantX;
 var CantY;
 
@@ -27,43 +28,118 @@ function calculateCant() {
     
     CantX--;
     CantY--;
+    
+    setInterval(nextPosition, 1000/15);
+    setFood();
 }
 
+function Pice(posX, posY) {
+    
+    this.posX = posX;
+    this.posY = posY;
+}
 
-var pice = {posX: 0, posY: 0};
+//var pice = {posX: 0, posY: 0};
+var pic = new Pice(0, 0);
 var snake = {size: 1, pices: []};
 
-snake.pices.push(pice);
-s/*nake.pices.push(pice = {posX: 1, posY: 0});*/
+var food = {posX: 0, posY: 0};
+
+snake.pices.push(pic);
+/*snake.pices.push(pice = {posX: 1, posY: 0});*/
 document.getElementById(snake.pices[0].posX + 'x' + snake.pices[0].posY).classList.add('snake');
+
+
+var keyEvent = "";
 
 function handle(event) {
 
+    keyEvent = event.key;
+}
+
+function nextPosition() {
+    
     document.getElementById(snake.pices[snake.pices.length - 1].posX + 'x' + snake.pices[snake.pices.length - 1].posY).classList.remove('snake');
     
-    for (i = snake.pices.length - 1; i > 1; i--) {
+    for (i = snake.pices.length - 1; i > 0; i--) {
+        
         snake.pices[i].posX = snake.pices[i - 1].posX;
         snake.pices[i].posY = snake.pices[i - 1].posY;
     }
     
-    if (event.key == 'ArrowUp'){
+    if (keyEvent == 'ArrowUp'){
         
-        if (snake.pices[0].posY != 0) snake.pices[0].posY--;
+        if (snake.pices[0].posY == 0) snake.pices[0].posY = CantY;
+        else snake.pices[0].posY--;
         
-    } else if (event.key == 'ArrowDown'){
+    } else if (keyEvent == 'ArrowDown'){
         
-        if (snake.pices[0].posY != CantY) snake.pices[0].posY++;
+        if (snake.pices[0].posY == CantY) snake.pices[0].posY = 0;
+        else snake.pices[0].posY++;
         
-    } else if (event.key == 'ArrowLeft'){
+    } else if (keyEvent == 'ArrowLeft'){
         
-        if (snake.pices[0].posX != 0) snake.pices[0].posX--;
+        if (snake.pices[0].posX == 0) snake.pices[0].posX = CantX;
+        else snake.pices[0].posX--;
         
-    } else if (event.key == 'ArrowRight'){
+    } else if (keyEvent == 'ArrowRight'){
         
-        if (snake.pices[0].posX != CantX) snake.pices[0].posX++;
+        if (snake.pices[0].posX == CantX) snake.pices[0].posX = 0;
+        else snake.pices[0].posX++;
         
-    }
+    } //else...
     
     document.getElementById(snake.pices[0].posX + 'x' + snake.pices[0].posY).classList.add('snake');
+    
+    rules();
+}
+
+function rules() {
+    
+    if ((snake.pices[0].posY == food.posY) && (snake.pices[0].posX == food.posX)) {
+        
+        var posX = snake.pices[snake.pices.length - 1].posX;
+        var posY = snake.pices[snake.pices.length - 1].posY;
+        
+        var newPice = new Pice(posX, posY);
+        
+        posX = snake.pices[snake.pices.length - 1].posX;
+        posY = snake.pices[snake.pices.length - 1].posY;
+        
+        snake.pices.push(newPice);
+    }
+}
+
+function setFood() {
+    
+    var posX, posY; 
+    var bool = true;
+    
+    while (bool) {
+        
+        posX = Math.floor((Math.random() * CantX));
+        posY = Math.floor((Math.random() * CantY));
+        
+        bool = false;
+        
+        for (i = snake.pices.length - 1; i > 0; i--) {
+            
+            if (snake.pices[i].posX == posX) {
+                
+                bool = true;
+                break;
+                
+            } else if (snake.pices[i].posY == posY) {
+                
+                bool = true;
+                break;
+            }
+        }
+    }
+    
+    food.posX = posX;
+    food.posY = posY;
+    
+    document.getElementById(food.posX + 'x' + food.posY).classList.add('food');
 }
 
